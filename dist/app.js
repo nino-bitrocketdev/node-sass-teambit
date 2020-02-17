33f9b64b04c19a5e71f3ce6d76e85baefea5e30d
Nino Chimento <saninoc84@gmail.com>
2020-02-17 14:19:11 +0100

    testo header

--- dist/app.js
+++ dist/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\sanin\OneDrive\Desktop\boolean\prova\src\app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! C:\Users\sanin\OneDrive\Desktop\boolean\prova\src\app.scss */"./src/app.scss");
+__webpack_require__(/*! C:\Users\sanin\OneDrive\Desktop\boolean\node-sass-teambit\src\app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! C:\Users\sanin\OneDrive\Desktop\boolean\node-sass-teambit\src\app.scss */"./src/app.scss");
 
 
 /***/ })
